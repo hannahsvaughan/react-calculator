d4d43c4d53976445d797b63e1afaef239a2512be
hannahsvaughan <hannahvaughan421@gmail.com>
2023-03-23 12:27:30 -0400

    components complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import KeyPadComponent from "./components/KeyPadComponent";
+import ResultComponent from "./components/ResultComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Constructor goes here
+  constructor() {
+    super();
+
+    this.state = {
+      result: "",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="calculator-body">
+          <h1>Simple React Calculator</h1>
+          {/* components go here once finished */}
+          <ResultComponent onClick={this.onClick} />
+          <KeyPadComponent onClick={this.onClick} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
